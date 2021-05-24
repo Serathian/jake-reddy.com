1a2d88e938f36d173a43a4a7d36e54a6ec233fcb
Jake <jake.a.reddy@gmail.com>
2021-05-24 10:08:51 +0300

    added test endpoint

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const app = express()
 app.use(cors())
 app.use(express.static('build'))
 
-const PORT = process.env.PORT || 3001
+app.get('/api/test', (req, res) => {
+  console.log('api/test called!')
+  res.send('This is test endpoint!').status(201)
+})
+
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
