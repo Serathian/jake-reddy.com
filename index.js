4ba33e2eda79960d03c68a5aa3b80bd00f9bebfd
Jake <jake.a.reddy@gmail.com>
2021-06-14 12:44:55 +0300

    added catchall endpoint to try and fix endpoint not found error

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const router = express.Router()
 const cors = require('cors')
 const app = express()
 const contactRouter = require('./controllers/contact')
+const path = require('path')
 
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
 app.use('/api/contact', contactRouter)
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, 'build')))
 
 //Should probably move this to its own module
 
 app.get('/api/test', (req, res) => {
   console.log('api/test called!')
   res.send('This is test endpoint!').status(201)
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
 const PORT = process.env.PORT || 3002
