8dd043b0e0d35670188d70ba47799eecff463549
Jake <jake.a.reddy@gmail.com>
2021-06-16 11:38:45 +0300

    fixed typo in catch all endpoint

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/api/test', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
 const PORT = process.env.PORT || 3002
